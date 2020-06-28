52fbada315fb5c6c96485c0dec3020c3e836fcf1
WHU-Traffic-Sign-Detection-Team <Ma_Yaowei@outlook.com>
2020-06-28 23:06:55 +0800

    finish the part of babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,17 @@
 const path = require("path")
 const webpack = require("webpack")
 module.exports = {
-    entry: "./src/client/index.js"
+    entry: "./src/client/index.js",
     // 并没有这js，我要创建一个
+
+    module: {
+        rules: [
+                {
+            test: '/\.js$/',
+            exclude: /node_modules/,
+            loader: "babel-loader"
+                }
+        ]
+    }
 }
+
