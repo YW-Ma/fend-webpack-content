ff28aa09f58d0fb4d34a02816f5c0c36eaac903a
WHU-Traffic-Sign-Detection-Team <Ma_Yaowei@outlook.com>
2020-06-29 23:09:39 +0800

    using prod and dev modes

---
+++ webpack.dev.js
@@ -0,0 +1,30 @@
+const path = require("path")
+const webpack = require("webpack")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+module.exports = {
+    entry: "./src/client/index.js",
+    // 并没有这js，我要创建一个
+
+    mode: "development",
+    module: {
+        rules: [
+            //添加一个规则：
+            // 从entry point出发，
+            // 对依赖关系中，所有不在node_modules里面的js文件
+            // 都利用babel-loader装载器来预处理
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "index.html"
+        })
+    ]
+}
+
--- webpack.prod.js
+++ webpack.prod.js
@@ -5,18 +5,8 @@ module.exports = {
     entry: "./src/client/index.js",
     // 并没有这js，我要创建一个
 
+    mode: "production",
     module: {
-        rules: [
-            //添加一个规则：
-            // 从entry point出发，
-            // 对依赖关系中，所有不在node_modules里面的js文件
-            // 都利用babel-loader装载器来预处理
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ]
     },
 
     plugins: [
